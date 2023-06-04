17a979265f4ed145bd7ac203bce52335dde63541
artherx <xxartherxx@gmail.com>
2023-06-04 16:42:21 -0500

    obtuve y cambios en el btn pa ocultar

--- src/routes/(centerINV)/testA/+page.svelte
+++ src/routes/(centerINV)/testA/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
     import '../../assets/css/revision_documentacion.css'
     import {goto} from '$app/navigation'
-    let aprobarD= false
-    let descartarD= false
-    let aprobarI= 'display: block;'
-    let descartarI= 'display: block;'
+    let aprobarD= true
+    let descartarD= true
     const onClickA=(e:Event)=>{
-        e.preventDefault(),
-        aprobarD=true
-        descartarI='display: none;'
+        if(aprobarD){
+            e.preventDefault(),
+            descartarD=false
+        }
     }
     const onClickD=(e:Event)=>{
-        e.preventDefault(),
-        descartarD=true
-        aprobarI='display: none;'
+        if(descartarD){
+            e.preventDefault(),
+            aprobarD=false
+        }
     }
 </script>
 <section class="contenido">
@@ -21,7 +21,7 @@
     <div id="carta2">
         <a href="" download="">nombre documento</a>
         <h6>4 1464 IN-IV-F-26 Concertación de entregables según OPS</h6>
-        <button id="aprobar" on:click={onClickA} >Aprobar</button>
-        <button id="denegar"on:click={onClickD} >Denegar</button>
+        <button id="aprobar" on:click={onClickA}  style={aprobarD?'': "opacity: 0;"}>Aprobar</button>
+        <button id="denegar"on:click={onClickD}  style={descartarD?'': "opacity: 0;"}>Denegar</button>
     </div>
 </section>
